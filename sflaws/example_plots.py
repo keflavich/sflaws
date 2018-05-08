5c325bb06dcb0db4cae4ad3b615400173f48bdcf
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-05-07 18:18:25 -0600

    udpate some plot stuff

--- sflaws/example_plots.py
+++ sflaws/example_plots.py
@@ -35,6 +35,10 @@ HC2011_multiff.color = 'g'
 
 if __name__ == "__main__":
 
+    from cycler import cycler
+
+    pl.rcParams['axes.prop_cycle'] = cycler('color', ("#"+x for x in ('338ADD', '9A44B6', 'A60628', '467821', 'CF4457', '188487', 'E24A33')))
+
     meandens = 1e4
     # xx is the density relative to the mean density, i.e., it is the overdensity
     xx = np.logspace(np.log10(meandens)-10, np.log10(meandens)+10, 10000) / meandens
@@ -60,13 +64,14 @@ if __name__ == "__main__":
 
             logmeandens = np.exp(-0.5 * thismodel.sigma_s**2)
 
-            ax.loglog(xx, lognormal(xx, sigma=thismodel.sigma_s))
+            L, = ax.loglog(xx, lognormal(xx, sigma=thismodel.sigma_s))
 
             thresh = np.exp(thismodel.scrit)
             print(pp, panels[pp], thismodel.sigma_s, thresh, np.log10(thresh), thismodel.scrit)
 
             ax.fill_between(xx[xx>thresh], xx[xx>thresh]*1e-100,
                             lognormal(xx[xx>thresh], sigma=thismodel.sigma_s),
+                            color=L.get_color(),
                             alpha=0.5)
 
             ax.vlines(logmeandens, 1e-10, 2, color='k', linestyle='--', linewidth=3,
@@ -75,16 +80,31 @@ if __name__ == "__main__":
             #ax.vlines(meandens, 1e-10, 2, color='k', linestyle='--', linewidth=1,
             #          alpha=0.5)
 
-            ax.set_title("$\mathcal{{M}}={mach} \\beta={beta}$".format(**panels[pp]))
-            ax.set_xlabel("Overdensity")
 
+            #print(pp, ax.get_subplotspec().num1, ax.get_subplotspec().num2)
+
+            ax.text(logmeandens/5e5, 5e-2, "$\mathcal{{M}}={mach}$\n$\\beta={beta}$".format(**panels[pp]))
             ax.axis([logmeandens/1e6, logmeandens*1e6,1e-6,1.5])
             ax.xaxis.set_major_locator(pl.matplotlib.ticker.LogLocator(base=100))
 
+            #ax.set_title("$\mathcal{{M}}={mach} \\beta={beta}$".format(**panels[pp]))
+            if pp == (3,2):
+                ax.set_xlabel("Overdensity")
+            if pp == (2,2):
+                ax.set_ylabel("$P(s)$")
+            if pp[0] in (1,2):
+                ax.set_xticklabels([])
+            if pp[1] in (2,3):
+                ax.set_yticklabels([])
+
+            ax.axis([logmeandens/1e6, logmeandens*1e6,1e-6,1.5])
+
         fig1.tight_layout()
+        fig1.subplots_adjust(hspace=0, wspace=0)
         pl.draw()
         pl.show()
         fig1.tight_layout()
+        fig1.subplots_adjust(hspace=0, wspace=0)
 
         fig1.savefig("{0}_machbetaplots.png".format(model.name), bbox_inches='tight')
 
@@ -93,11 +113,14 @@ if __name__ == "__main__":
         Mach = np.linspace(1, 100)
         Beta = (1.25, 2, np.inf)
 
-        for bb,linestyle in zip(Beta, (':','--','-')):
+        #for bb,linestyle in zip(Beta, (':','--','-')):
+        # only plot no-b-field instance
+        for bb,linestyle in [(np.inf, '-')]:
             thismodel = model(Mach=Mach, Beta=bb)
             ax.plot(Mach, thismodel.scrit, color=thismodel.color,
                     linestyle=linestyle,
-                    label="{0} $\\beta={1}$".format(thismodel.name, bb))
+                    #label="{0} $\\beta={1}$".format(thismodel.name, bb))
+                    label="{0}".format(thismodel.name,))
 
             if thismodel.color == 'm' and linestyle == ':':
                 print("doot")
